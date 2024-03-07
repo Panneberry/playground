8435e8ca49e33da734127eb2c56371bf127bf74f
Panneberry <pannenarumon@gmail.com>
2024-03-07 22:58:51 +0700

    first 2 boxes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Calculator() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (event) => {
+    setNum1(event.target.value);
+  };
+
+  const handleNum2Change = (event) => {
+    setNum2(event.target.value);
+  };
+
+  const handleCalculate = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" value={num1} onChange={handleNum1Change} />
+      <span> + </span>
+      <input type="number" value={num2} onChange={handleNum2Change} />
+      <button onClick={handleCalculate}>=</button>
+      <span>{result}</span>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
